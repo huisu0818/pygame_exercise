4449a688adaa2eb137292f9a2910f4061da77ef8
Huisu Lee (DeskTop) <huisu000818@gmail.com>
2023-03-05 20:50:22 +0900

    commit_2

--- pygame_test/2_background.py
+++ pygame_test/2_background.py
@@ -13,11 +13,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Huisu Game") # 게임 이름
 
 # 배경 이미지 불러오기
-background = pygame.image.load("/Users/huisu/Desktop/Code_manager/개인 프로젝트/python_programing/오락실 게임/background.png")
-
-# 캐릭터(스프라이트) 불러오기
-character = pygame.image.load("/Users/huisu/Desktop/Code_manager/개인 프로젝트/python_programing/오락실 게임/background.png")
-
+background = pygame.image.load("C:/Users/pc/Desktop/code_manager(DeskTop)/개인 프로젝트/pygame_exercise/pygame_test/background.png")
 
 # 이벤트 루프 
 running = True # 게임이 진행여부
@@ -26,8 +22,9 @@ while running:
         if event.type == pygame.QUIT: # 창이 닫히는 이벤트 발생 여부
             running = False
 
+    # screen.fill((0, 0, 255))
     screen.blit(background, (0, 0)) # 배경 그리기
     pygame.display.update() # 게임화면을 계속 그려줌
 
 # pygame 종료
-pygame.quit()
+pygame.quit() 
---
+++ pygame_test/4_keyboard_event.py
@@ -0,0 +1,71 @@
+import pygame
+
+pygame.init() # 초기화 (필수)
+
+
+# 화면 크기 설정
+screen_width = 480 # 가로 크기
+screen_height = 640 # 세로 키기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Huisu Game") # 게임 이름
+
+# 배경 이미지 불러오기
+background = pygame.image.load("C:/Users/pc/Desktop/code_manager(DeskTop)/개인 프로젝트/pygame_exercise/pygame_test/background.png")
+
+# 캐릭터 (스프라이트) 불러오기
+character = pygame.image.load("C:/Users/pc/Desktop/code_manager(DeskTop)/개인 프로젝트/pygame_exercise/pygame_test/character.png")
+character_size = character.get_rect().size # 이미지의 크기를 구해옴
+character_width = character_size[0] # 캐릭터 가로 크기
+character_height = character_size[1] # 캐릭터 세로 크기
+character_x_pos = (screen_width - character_width) / 2 # 화면 가로의 절반 크기에 해당하는 곳에 위치 (가로 위치)
+character_y_pos = screen_height - character_height # 화면 세로 크기 가장 아래에 해당하는 곳에 위치 (세로 위치)
+
+# 이동한 좌표
+to_x = 0
+to_y = 0
+
+# 이벤트 루프 
+running = True # 게임이 진행여부
+while running:
+    for event in pygame.event.get(): # 어떤 이벤트가 발생하는지
+        if event.type == pygame.QUIT: # 창이 닫히는 이벤트 발생 여부
+            running = False
+
+        if event.type == pygame.KEYDOWN: # 키보드 입력 확인
+            if event.key == pygame.K_LEFT:
+                to_x -= 5
+            elif event.key == pygame.K_RIGHT:
+                to_x += 5
+            elif event.key == pygame.K_UP:
+                to_y -= 5
+            elif event.key == pygame.K_DOWN:
+                to_y += 5
+
+        if event.type == pygame.KEYUP: # 방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+    
+    character_x_pos += to_x
+    character_y_pos += to_y
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
+
+    screen.blit(background, (0, 0)) # 배경 그리기
+
+    screen.blit(character, (character_x_pos, character_y_pos))
+    pygame.display.update() # 게임화면을 계속 그려줌
+
+# pygame 종료
+pygame.quit()
